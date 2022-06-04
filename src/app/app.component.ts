9ff9405bd8c3815456fd3b488c31075027f7ef4c
macc001 <miguelcayocarvajal37@gmail.com>
2022-06-03 23:23:24 -0400

    se agrego formArrar xon listado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,7 @@ export class AppComponent {
   form: FormGroup;
   listShoppingForm: FormGroup;
   shoppingList = [
-    { id: 1, price: 2 },
-    { id: 2, price: 32 },
+    { id: '2', price: 32 },
     { id: 3, price: 30 },
   ];
 
@@ -23,7 +22,7 @@ export class AppComponent {
     this.listShoppingForm = fb.group({
       shopping: fb.array([]),
     });
-    this.addShopping();
+    this.loadShopping();
   }
 
   addNewAddressGroup() {
@@ -56,18 +55,91 @@ export class AppComponent {
   }
 
   // SHOPPING
-  addShopping() {
+  loadShopping() {
     this.shoppingList.forEach((item) => {
       const add = this.listShoppingForm.get('shopping') as FormArray;
       add.push(
         this.fb.group({
-          item: '0',
+          item: item.price,
         })
       );
     });
   }
 
-  changeInputShopping() {
+  newShopping() {
+    const add = this.listShoppingForm.get('shopping') as FormArray;
+    add.push(
+      this.fb.group({
+        item: '1',
+      })
+    );
+    const uid = this.uidV1();
+    this.shoppingList.push({ id: uid, price: 1 });
+  }
+
+  changeInputShopping(i: any) {
+    console.log(i);
     console.log(this.listShoppingForm.value);
+    console.log(this.shoppingList);
+  }
+
+  subtractShopping(index: number, id: number) {
+    const add = this.listShoppingForm.get('shopping') as FormArray;
+    const valueItem = add.at(index).value;
+    add
+      .at(index)
+      .get('item')
+      .patchValue(Number(valueItem.item) - 1);
+    const newValue = add.at(index).value;
+    this.shoppingList = this.shoppingList.map((item) => {
+      if (item.id == id) {
+        return {
+          ...item,
+          price: newValue.item,
+        };
+      }
+      return item;
+    });
+  }
+
+  addShopping(index: number, id: number) {
+    const add = this.listShoppingForm.get('shopping') as FormArray;
+    const valueItem = add.at(index).value;
+    add
+      .at(index)
+      .get('item')
+      .patchValue(Number(valueItem.item) + 1);
+    const newValue = add.at(index).value;
+    this.shoppingList = this.shoppingList.map((item) => {
+      if (item.id == id) {
+        return {
+          ...item,
+          price: newValue.item,
+        };
+      }
+      return item;
+    });
+  }
+
+  deleteShopping(index: number, id: number) {
+    const add = this.listShoppingForm.get('shopping') as FormArray;
+    add.removeAt(index);
+    this.shoppingList = this.shoppingList.filter((item) => item.id !== id);
+  }
+
+  //GENERATE UID
+  private uidV1() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+      /[xy]/g,
+      function (c) {
+        var r = (Math.random() * 16) | 0,
+          v = c == 'x' ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      }
+    );
+  }
+
+  private uidV2() {
+    return Date.now().toString(36) + Math.random().toString(36).substring(2);
   }
 }
